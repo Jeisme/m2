75b10839e875a239a06a4ee4b50e5f4cf9035434
Je <48697021@qq.com>
2019-06-22 19:46:58 +0800

    1.用户创建

---
+++ git_Demo/src/main/java/com/itheima/dao/userDao.java
@@ -0,0 +1,13 @@
+package com.itheima.dao;
+
+/**
+ * @author ：Suming
+ * @date ：Created in 2019/6/22 19:19
+ * @description ：
+ * @version: 1.0
+ */
+public class userDao {
+    private String username;
+    private String password;
+    private Integer age;
+}
